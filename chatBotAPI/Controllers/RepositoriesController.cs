8cf169ce726de93bf46cbd1593bcb9d98e7ce4c6
Priscila Ramos de Luna <pri@MacBook-Air-de-Priscila.local>
2024-12-27 17:56:16 -0300

    commit inicial

---
+++ chatBotAPI/Controllers/RepositoriesController.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RepositoriesController : ControllerBase
+{
+    private readonly HttpClient _httpClient;
+
+    public RepositoriesController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (compatible; Bot/1.0)");
+    }
+
+    [HttpGet("csharp-repositories")]
+    public async Task<IActionResult> GetCSharpRepositories()
+    {
+        try
+        {
+            var url = "https://api.github.com/users/takenet/repos";
+
+            var response = await _httpClient.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                return StatusCode((int)response.StatusCode, "Erro ao acessar a API do GitHub.");
+            }
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var repositories = System.Text.Json.JsonSerializer.Deserialize<List<Repository>>(jsonResponse);
+
+            if (repositories == null || !repositories.Any())
+            {
+                return Ok("Nenhum repositório encontrado.");
+            }
+
+            var csharpRepositories = repositories
+            .Where(repo => repo.Language == "C#") 
+            .OrderBy(repo => repo.CreatedAt)      
+            .Take(5)                              
+            .Select(repo => new                   
+            {
+                repo.FullName,
+                repo.Description,
+                repo.Language,
+                repo.CreatedAt,
+                AvatarUrl = repo.Owner.AvatarUrl
+            });
+            return Ok(csharpRepositories);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Erro interno: {ex.Message}");
+        }
+    }
+
+}
+
---
+++ chatBotAPI/Models/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+public class Repository
+{
+    [JsonPropertyName("full_name")]
+    public string FullName { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("language")]
+    public string Language { get; set; }
+
+    [JsonPropertyName("owner")]
+    public Owner Owner { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+}
+
+public class Owner
+{
+    [JsonPropertyName("avatar_url")]
+    public string AvatarUrl { get; set; }
+}
